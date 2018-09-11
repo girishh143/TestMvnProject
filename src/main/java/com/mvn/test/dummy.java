f875fac3790019c1903b920322eae11488199233
girishh143 <girishk@synerzipune.local>
2018-09-11 18:49:35 +0530

    Added testng.xml file..

---
+++ src/main/java/com/mvn/test/dummy.java
@@ -0,0 +1,10 @@
+package com.mvn.test;
+
+public class dummy {
+	
+	
+	public void dummy11(){
+		System.out.println("IN dummy Test");
+	}
+
+}
--- src/test/java/com/mvn/test/abcd.java
+++ src/test/java/com/mvn/test/abcd.java
@@ -1,7 +1,5 @@
 package com.mvn.test;
 
-import java.sql.Driver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
