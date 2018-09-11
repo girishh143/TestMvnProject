9a25e3d7f0f8c8673123a5be99cfda7467501df7
Girish Kore <girishk@synerzipune.local>
2018-09-11 11:29:15 +0530

    first commit..

---
+++ src/main/java/com/mvn/test/abcd.java
@@ -0,0 +1,51 @@
+package com.mvn.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class abcd {
+
+	
+	@Test(priority=1)
+	public void demo() throws InterruptedException{
+		
+		System.setProperty("webdriver.gecko.driver", "E:\\MealsPlus\\WorkSpaceMealsPlus\\Test\\Driver\\geckodriver.exe");
+		
+		for(int i=0;i<4;i++){
+		System.out.println("Testing......!!!!!!!!!!");
+		Thread.sleep(1000);
+		}
+		
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		Thread.sleep(3000);
+		System.out.println("Url Tital:"+driver.getTitle());
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("admin");
+		System.out.println("UserName entered...!!!");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		System.out.println("Password Entered...!!!");
+		driver.findElement(By.id("btnLogin")).click();
+		System.out.println("Login Successful...!!!");
+		System.out.println("Tital after Login:"+driver.getTitle());
+		
+		
+	}
+	
+	@Test(priority=2)
+	public void demo2(){
+		System.out.println("priority 2 test case get executed...!!!");
+		
+	}
+	
+	
+
+	@Test(priority=3)
+	public void demo3(){
+		System.out.println("priority 3 test case get executed...!!!");
+		
+	}
+}
