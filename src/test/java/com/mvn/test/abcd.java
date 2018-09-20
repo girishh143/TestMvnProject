94a0ed58463be00d9600b969dd15a29623787d6a
Girish Kore <girish.kore@synerzip.com>
2018-09-20 15:25:13 +0530

    Added the headless driver in script...git add .git add .

--- src/test/java/com/mvn/test/abcd.java
+++ src/test/java/com/mvn/test/abcd.java
@@ -1,23 +1,31 @@
 package com.mvn.test;
 
+import java.io.File;
+
+import javax.swing.text.Utilities;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
-import junit.framework.Assert;
 
 public class abcd {
 
-	//WebDriver driver;
-	HtmlUnitDriver  driver;
+	WebDriver driver;
+	
 	@Test(priority=1)
 	public void demo() throws InterruptedException{
+	//	System.setProperty("webdriver.gecko.driver", "E:\\MealsPlus\\WorkSpaceMealsPlus\\Test\\Driver\\geckodriver.exe");
 		
-		System.setProperty("webdriver.gecko.driver", "E:\\MealsPlus\\WorkSpaceMealsPlus\\Test\\Driver\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+                //Option for Windows OS
+		options.addArguments("--disable-gpu");
+		driver = new ChromeDriver(options);
 		
-		for(int i=0;i<4;i++){
+		for(int i=0;i<2;i++){
 		System.out.println("Testing......!!!!!!!!!!");
 		Thread.sleep(500);
 		}
@@ -25,28 +33,22 @@ public class abcd {
 		System.out.println("priority 1 test case get executed...!!!");
 		 
 		//driver = new FirefoxDriver();
-		driver = new HtmlUnitDriver();
-		
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		Thread.sleep(3000);
-		//System.out.println("Url Tital: "+driver.getTitle());
-		
+		System.out.println("Url Tital: "+driver.getTitle());
 		driver.findElement(By.id("txtUsername")).sendKeys("admin");
 		System.out.println("UserName entered...!!!");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		System.out.println("Password Entered...!!!");
 		driver.findElement(By.id("btnLogin")).click();
 		System.out.println("Login Successful...!!!");
-		//System.out.println("Tital after Login:"+driver.getTitle());
+		System.out.println("Tital after Login:"+driver.getTitle());
 		 
-		
 	}
 	
 	@Test(priority=2)
 	public void demo2(){
 		System.out.println("priority 2 test case get executed...!!!");
-		//driver.findElement(By.xpath("ddd")).click();
-		
 	}
 	
 	@Test(priority=3)
